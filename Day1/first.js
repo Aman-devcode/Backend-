08bff04ed3a8d55faa36b78eadefdd1cec36c407
Aman kumar pandit <amanbr320603@gmail.com>
2026-09-12 03:07:23 +0530

    Day1

---
+++ Day1/first.js
@@ -0,0 +1,4 @@
+const{sum,sub}=require("./second")
+sum(5,6);
+sub(5,6);
+console.log("hellow aman kaise ho bhai");
---
+++ Day1/second.js
@@ -0,0 +1,11 @@
+console.log("hi mein thik hu");
+function sum (a,b){
+    console.log(a+b)
+}
+function sub(a,b){
+    console.log(a-b)
+}
+module.exports={sum:sum ,sub:sub}
+
+// CJS:common js module 
+// it is old method 
