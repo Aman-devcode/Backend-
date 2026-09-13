e610d2d2b60bbf2811377d05d9839f8ad4e8baa0
Aman kumar pandit <amanbr320603@gmail.com>
2026-09-13 13:44:01 +0530

    Day2

---
+++ Day2/first.js
@@ -0,0 +1,5 @@
+const {sum,sub,mul}=require("./current")
+sum(2,4);
+sub(4,2);
+mul(4,5);
+console.log("hello aman ji");
---
+++ Day2/second.mjs
@@ -0,0 +1,5 @@
+console.log("hellow aman ")
+function sum(a,b){
+    console.log(a+b);
+}
+export default sum;
